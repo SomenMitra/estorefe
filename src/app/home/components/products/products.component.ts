f98e845fac231e8529451ceb6fb9b3c5e5d78fdb
Somen Mitra <somenmitra22082000@gmail.com>
2025-01-22 15:40:56 +0530

    some modifications

--- src/app/home/components/products/products.component.ts
+++ src/app/home/components/products/products.component.ts
@@ -7,23 +7,33 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { Product } from '../../types/products.types';
 import { CartStoreItem } from '../../services/cart/cart.storeItem';
-
-
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule, SharedModule,RouterModule],
+  imports: [CommonModule, FontAwesomeModule, SharedModule, RouterModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
   faShoppingCart = faShoppingCart;
-  constructor(public productStore: ProductStoreItem, private cart: CartStoreItem) {}
+  constructor(
+    public productStore: ProductStoreItem,
+    private cart: CartStoreItem
+  ) {}
 
-
-  addToCart(product:Product){
+  addToCart(product: Product) {
     this.cart.addProduct(product);
-  }
 
+    Swal.fire({
+      title: 'Added to Cart',
+      text: `${product.product_name} has been added to your cart.`,
+      icon: 'success',
+      timer: 2000, // Automatically close after 2 seconds
+      showConfirmButton: false,
+      position: 'top-end',
+      toast: true,
+    });
+  }
 }
